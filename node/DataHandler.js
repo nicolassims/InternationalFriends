4bd81b3b2aecb6de2809c5b079c8478f7f2ac6ff
Nicolas Sims <sableye.nico@gmail.com>
2017-05-07 07:51:48 -0400

    Finished register process

--- node/DataHandler.js
+++ node/DataHandler.js
@@ -3,6 +3,7 @@
 const IO = require('fs');
 
 let comments = [];
+let users = [];
 
 class DataHandler {
     static refreshComments() {
@@ -16,6 +17,18 @@ class DataHandler {
             comments.unshift(data);
         }
         return comments;
+    }
+
+    static handleUserSignup(data) {
+        data = JSON.parse(data);
+        data = data.toString().split(/,/);
+        for (let i = 0; i < users.length; i++) {
+            if (data[0] == users[i][0]) {
+                return 1;
+            }
+        }
+        users[users.length] = data;
+        console.log(users);
     }
 }
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,10 +6,51 @@
 
 'use strict';
 
+let activeuser;
+
 class main {
     constructor() {
+        main.hidePages();
         main.refreshComments();
         main.addComment();
+        main.switchPages();
+        main.handleSignup();
+    }
+
+    static hidePages() {
+        document.getElementById('signupPage').style.display = "none"
+    }
+
+    static handleSignup() {
+        document.getElementById('signupSubmit').addEventListener('click', () => {
+            if (document.getElementById('signupPassword').value === '' || document.getElementById('signupEmail').value === '' || ! /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById('signupEmail').value)) {
+                alert(`You must provide a proper email address and password to continue.`)
+            } else if (document.getElementById('signupPassword').value != document.getElementById('confirmPassword').value) {
+                alert(`Your passwords don't match. Want to fix that?`)
+            } else {
+                main.performAjax('XMLHttpRequest2', JSON.stringify([document.getElementById('signupEmail').value, document.getElementById('signupPassword').value, document.getElementById('username').value]), (response) => {
+                    if (response == '1') {
+                        alert('There\'s already a user with that email address.')
+                    } else {
+                        document.getElementById('signupPage').style.display = "none";
+                        document.getElementById('mainPage').style.display = "block";
+                        alert('Welcome to I.F., ' + document.getElementById('username').value + '!');
+                        activeuser = document.getElementById('signupEmail').value + ',' + document.getElementById('username').value;
+                        document.getElementById('signupEmail').value = '';
+                        document.getElementById('signupPassword').value = '';
+                        document.getElementById('confirmPassword').value = '';
+                        document.getElementById('username').value = '';
+                    }
+                });
+            }
+        });
+    }
+
+    static switchPages() {
+        document.getElementById('registerButton').addEventListener('click', () => {
+            document.getElementById('signupPage').style.display = "block";
+            document.getElementById('mainPage').style.display = "none"
+        });
     }
 
     static refreshComments() {
@@ -30,7 +71,8 @@ class main {
             if (document.getElementById('commentText').value == '') {
                 alert('Cat got your tongue?');
             } else {
-                let sendaway = '<div>' + document.getElementById('commentText').value + '</div><br>';
+                let sendaway = '<div>' + document.getElementById('commentText').value.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/'/g, "&#039;").replace(/\\/g, "&#92;") + '</div><br>';
+                document.getElementById('commentText').value = '';
                 main.performAjax('XMLHttpRequest1', JSON.stringify(sendaway), (response) => {
                     response = response.replace(/"]/g, '').replace(/\["/g, '').replace(/\\"/g, '').replace(/","/g, '');
                     document.getElementById('comments').innerHTML = response;
